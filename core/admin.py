42a2ade7ce2142d1b46863f380da291f1b0a26c2
ZooBycH <mz88@list.ru>
2022-12-10 11:33:12 +0300

    dev33 complete

---
+++ core/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    fieldsets = (
+        (
+            None,
+            {'fields': ('username', 'password')}
+        ),
+        (
+            'Personal info',
+            {'fields': ('first_name', 'last_name', 'email')}
+        ),
+        (
+            'Permissions',
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (
+            'Other info',
+            {'fields': ('last_login', 'date_joined')}
+        ),
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
