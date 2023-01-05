fa77d8fa4bb3838190a8abf8cf2d54ca20b0f192
ZooBycH <mz88@list.ru>
2023-01-06 02:06:27 +0300

    Board

--- goals/admin.py
+++ goals/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
-from goals.models import GoalCategory
+from goals.models import GoalCategory, Goal, GoalComment, Board
 
 
 class GoalCategoryAdmin(admin.ModelAdmin):
     list_display = ("title", "user", "created", "updated")
-    search_fields = ("title", "user")
+    search_fields = ("title", "user", "board")
 
 
 admin.site.register(GoalCategory, GoalCategoryAdmin)
+admin.site.register(Goal)
+admin.site.register(GoalComment)
+admin.site.register(Board)
